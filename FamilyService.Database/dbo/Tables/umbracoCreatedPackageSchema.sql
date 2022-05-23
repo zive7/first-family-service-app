8e8de45b244ff9f87f619a478377382e5712b44a
Zhivko Stojanov <zis@mca.dev>
2022-05-23 17:49:01 +0200

    initial commit

---
+++ FamilyService.Database/dbo/Tables/umbracoCreatedPackageSchema.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[umbracoCreatedPackageSchema] (
+    [id]         INT              IDENTITY (1, 1) NOT NULL,
+    [name]       NVARCHAR (255)   NOT NULL,
+    [value]      NVARCHAR (MAX)   NOT NULL,
+    [updateDate] DATETIME         CONSTRAINT [DF_umbracoCreatedPackageSchema_updateDate] DEFAULT (getdate()) NOT NULL,
+    [packageId]  UNIQUEIDENTIFIER NOT NULL,
+    CONSTRAINT [PK_umbracoCreatedPackageSchema] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_umbracoCreatedPackageSchema_Name]
+    ON [dbo].[umbracoCreatedPackageSchema]([name] ASC);
+
---
+++ FamilyService.Database/dbo/Tables/umbracoTwoFactorLogin.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[umbracoTwoFactorLogin] (
+    [id]              INT              IDENTITY (1, 1) NOT NULL,
+    [userOrMemberKey] UNIQUEIDENTIFIER NOT NULL,
+    [providerName]    NVARCHAR (400)   NOT NULL,
+    [secret]          NVARCHAR (400)   NOT NULL,
+    CONSTRAINT [PK_umbracoTwoFactorLogin] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_umbracoTwoFactorLogin_userOrMemberKey]
+    ON [dbo].[umbracoTwoFactorLogin]([userOrMemberKey] ASC);
+
+
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_umbracoTwoFactorLogin_ProviderName]
+    ON [dbo].[umbracoTwoFactorLogin]([providerName] ASC, [userOrMemberKey] ASC);
+
